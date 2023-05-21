4d51d5bb98ff00dfeadf323f755d69ae8a7e0890
Gustav <gustavmangion@hotmail.com>
2023-05-21 18:55:28 +0400

    Link input and button to refresh grid

--- etchsketch.js
+++ etchsketch.js
@@ -1,6 +1,16 @@
 const grid = document.querySelector(".grid");
 
-function addGridChildren(n) {}
+function addGridChildren(n) {
+	for (let i = 0; i < n; i++) {
+		const row = document.createElement("div");
+		for (let j = 0; j < n; j++) {
+			const row_child = document.createElement("div");
+			row_child.className = "grid-child";
+			row.appendChild(row_child);
+		}
+		grid.appendChild(row);
+	}
+}
 
 function clearGrid() {
 	while (grid.firstChild) {
@@ -8,4 +18,15 @@ function clearGrid() {
 	}
 }
 
-addGridChildren(2);
+const resizeBtn = document.getElementById("grid-size-confirm");
+resizeBtn.addEventListener("click", updateGrid);
+
+const gridSizeInput = document.getElementById("grid-size");
+
+function updateGrid() {
+	let newGridSize = gridSizeInput.value;
+	clearGrid();
+	addGridChildren(newGridSize);
+}
+
+updateGrid();
